16fca4bbed1fc63330eed50009cc6e6abaade036
Ananth Bhat <ananth.bhat94@gmail.com>
2017-08-16 15:35:46 -0700

    Update MemoryController.sv
    Formatting fixed

--- MemoryController.sv
+++ MemoryController.sv
@@ -31,7 +31,7 @@ logic [1:0] PageHit;
 logic PageMiss, PageEmpty;
 logic valid;
 
-////////////////////BUFFER///////////////////////////////////
+/////////////////////////BUFFER///////////////////////////////////
 
 //A buffer is created to store the result of a read
 //0The upper 512 MSB is the Data, the lower 3 bits are the tag
@@ -53,42 +53,39 @@ assign row = Address_sf [31:17];
 assign bank = Address_sf[16:15];
 assign column = {Address_sf[14:8],Address_sf[5:3]};
 assign bankgroup = Address_sf[7:6];
-/*
-initial 
-$monitor("R:%x\tB:%x\tC:%x",row,bank,column);
-*/
 
+//Instantiate all necessary mdoules
 //Instantiate the FSM
 MemContFSM m( 	.start, .rw(rw_sf), .refresh, .ap,
-				.bank,.bankgroup,
-				.row,.column,
-				.datawrite(Data_sf), .dataread(databuff), .buff,.tagreadin,.tagreadout,
-				.DDR4Bus(DDR4Bus),
-				.reset,.clock,.clock2x,.done,
-				.PageHit,.PageEmpty,.PageMiss
-			);
+		.bank,.bankgroup,
+		.row,.column,
+		.datawrite(Data_sf), .dataread(databuff), .buff,.tagreadin,.tagreadout,
+		.DDR4Bus(DDR4Bus),
+		.reset,.clock,.clock2x,.done,
+		.PageHit,.PageEmpty,.PageMiss
+ 	    );
 
 //Instantiate the Scheduler
 Scheduler S (	.DataIn(Data_cs),.AddrIn(Address_cs), 
-				.rw_in(rw_cs),.reset,.clock,.valid,
-				.DataOut(Data_sf), .AddrOut(Address_sf),
-				.rw_out(rw_sf),.full,.refresh,.ap,.tag,
-				.start,.done,.tagread(tagreadin),.tagfsm(tagreadout),
-				.buff,.PageHit,.PageMiss,.PageEmpty
-			);
+		.rw_in(rw_cs),.reset,.clock,.valid,
+		.DataOut(Data_sf), .AddrOut(Address_sf),
+		.rw_out(rw_sf),.full,.refresh,.ap,.tag,
+		.start,.done,.tagread(tagreadin),.tagfsm(tagreadout),
+		.buff,.PageHit,.PageMiss,.PageEmpty
+	    );
 
-//Interface to the Memory Controller receiver that receives from cache 
-receive_command rc 	(	.IF(MCRx),.data_out(Data_cs), 
-						.addr_out(Address_cs), .valid,.rw_out(rw_cs),
-						.tag_gen(tag),.fifo_full(full)
-					);
+//Interface to the Memory Controller receiver that receives requests from cache 
+receive_command rc (	.IF(MCRx),.data_out(Data_cs), 
+			.addr_out(Address_cs), .valid,.rw_out(rw_cs),
+			.tag_gen(tag),.fifo_full(full)
+		   );
 
 //Interface to the Memory Controller that sends data to cache
-send_read_data srd 	(	.IF(MCTx), 
-						.data_in(ReadBuffer[514:3]), 
-						.tag_gen(ReadBuffer[2:0]),
-						.internal_ready(buff)
-					);
+send_read_data srd (	.IF(MCTx), 
+			.data_in(ReadBuffer[514:3]), 
+			.tag_gen(ReadBuffer[2:0]),
+			.internal_ready(buff)
+		   );
 
 endmodule
 
